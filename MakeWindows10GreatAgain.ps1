1c4b9117c90727beb6e5cd31223fb42aa5b26215
SomeBadCoding <11685586+SomeBadCoding@users.noreply.github.com>
2019-06-22 18:14:21 -0600

    Just a Little Update

--- MakeWindows10GreatAgain.ps1
+++ MakeWindows10GreatAgain.ps1
@@ -99,6 +99,9 @@ Set-ItemProperty -Path 'hklm:\SYSTEM\CurrentControlSet\Services\AFD\Parameters' 
 #Disables Cached Logon Credential
 Set-ItemProperty -Path 'hklm:\Software\Microsoft\Windows Nt\CurrentVersion\Winlogon' -Name "CachedLogonsCount" -Value "0" 
 
+#Remove Hello-Face
+Get-WindowsPackage -Online | Where PackageName -like *Hello-Face* | Remove-WindowsPackage -Online -NoRestart
+
 #Restrict Null Sessions
 Set-ItemProperty -Path 'hklm:\SYSTEM\CurrentControlSet\Control\LSA' -Name "RestrictAnonymous" -Value "00000001" 
 Set-ItemProperty -Path 'hklm:\SYSTEM\CurrentControlSet\Control\LSA' -Name "everyoneincludesanonymous" -Value "00000000" 
@@ -111,13 +114,31 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RunAsPPL /t REG_DWORD /d 00000001 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest" /v UseLogonCredential /t REG_DWORD /d 0 /f
 
+#No more forced updates
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AUOptions /t REG_DWORD /d 2 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v ScheduledInstallDay /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v ScheduledInstallTime /t REG_DWORD /d 3 /f
+
+#No license checking
+reg add "HKLM\Software\Policies\Microsoft\Windows NT\CurrentVersion\Software Protection Platform" /v NoGenTicket /t REG_DWORD /d 1 /f
+#Disable sync
+reg add "HKLM\Software\Policies\Microsoft\Windows\SettingSync" /v DisableSettingSync /t REG_DWORD /d 2 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\SettingSync" /v DisableSettingSyncUserOverride /t REG_DWORD /d 1 /f
+
+#No Windows Tips
+reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v DisableSoftLanding /t REG_DWORD /d 1 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightFeatures /t REG_DWORD /d 1 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /t REG_DWORD /d 1 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\DataCollection" /v DoNotShowFeedbackNotifications /t REG_DWORD /d 1 /f
+reg add "HKLM\Software\Policies\Microsoft\WindowsInkWorkspace" /v AllowSuggestedAppsInWindowsInkWorkspace /t REG_DWORD /d 0 /f
+
 # Block Certain Applications from Outbound Connections
 NetSh Advfirewall set allprofiles state on
 Netsh.exe advfirewall firewall add rule name="Block Notepad.exe netconns" program="%systemroot%\system32\notepad.exe" protocol=tcp dir=out enable=yes action=block profile=any
 Netsh.exe advfirewall firewall add rule name="Block regsvr32.exe netconns" program="%systemroot%\system32\regsvr32.exe" protocol=tcp dir=out enable=yes action=block profile=any
 Netsh.exe advfirewall firewall add rule name="Block calc.exe netconns" program="%systemroot%\system32\calc.exe" protocol=tcp dir=out enable=yes action=block profile=any
 Netsh.exe advfirewall firewall add rule name="Block mshta.exe netconns" program="%systemroot%\system32\mshta.exe" protocol=tcp dir=out enable=yes action=block profile=any
-Netsh.exe advfirewall firewall add rule name="Block wscript.exe netconns" program="%systemroot%\system32\wscript.exe" protocol=tcp dir=out enable=yes action=block profile=any
 Netsh.exe advfirewall firewall add rule name="Block cscript.exe netconns" program="%systemroot%\system32\cscript.exe" protocol=tcp dir=out enable=yes action=block profile=any
 Netsh.exe advfirewall firewall add rule name="Block runscripthelper.exe netconns" program="%systemroot%\system32\runscripthelper.exe" protocol=tcp dir=out enable=yes action=block profile=any
 
@@ -125,6 +146,8 @@ Netsh.exe advfirewall firewall add rule name="Block runscripthelper.exe netconns
 netsh advfirewall set currentprofile logging filename %systemroot%\system32\LogFiles\Firewall\pfirewall.log
 netsh advfirewall set currentprofile logging maxfilesize 4096
 netsh advfirewall set currentprofile logging droppedconnections enable
+
+Write-Host "Remove Apps"
 
 # Remove Default Apps
 Get-AppxPackage "Microsoft.3DBuilder" | Remove-AppxPackage
@@ -162,6 +185,9 @@ Get-AppxPackage "Microsoft.Windows.Photos" | Remove-AppxPackage
 Get-AppxPackage "Microsoft.WindowsSoundRecorder" | Remove-AppxPackage
 Get-AppxPackage "Microsoft.ZuneMusic" | Remove-AppxPackage
 Get-AppxPackage "Microsoft.ZuneVideo" | Remove-AppxPackage
+Get-AppxPackage "maps" | Remove-AppxPackage
+Get-AppxPackage "alarms" | Remove-AppxPackage
+Get-AppxPackage "people" | Remove-AppxPackage
 
 # Unistall 3rd Party Apps
 Get-AppxPackage "2414FC7A.Viber" | Remove-AppxPackage
@@ -193,3 +219,141 @@ Get-AppxPackage "PandoraMediaInc.29680B314EFC2" | Remove-AppxPackage
 Get-AppxPackage "SpotifyAB.SpotifyMusic" | Remove-AppxPackage
 Get-AppxPackage "WinZipComputing.WinZipUniversal" | Remove-AppxPackage
 Get-AppxPackage "XINGAG.XING" | Remove-AppxPackage
+
+# Block Microsoft Domains
+
+Write-Host "Edit Local Hosts"
+
+$file = "C:\Windows\System32\drivers\etc\hosts"
+"127.0.0.1 vortex-win.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telecommand.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telecommand.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 oca.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 oca.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sqm.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sqm.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 redir.metaservices.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 choice.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 choice.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 reports.wes.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 services.wes.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sqm.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.ppe.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.appex.bing.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.urs.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.appex.bing.net:443" | Out-File -encoding ASCII -append $file
+"127.0.0.1 vortex-sandbox.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 settings-sandbox.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 vortex.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 vortex-win.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telecommand.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telecommand.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 oca.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 oca.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sqm.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sqm.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 redir.metaservices.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 choice.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 choice.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 vortex-sandbox.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 settings-sandbox.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 reports.wes.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sqm.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.ppe.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 wes.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.appex.bing.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.urs.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 survey.watson.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.live.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 services.wes.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.appex.bing.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 vortex.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 vortex-win.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telecommand.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telecommand.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 oca.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 oca.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sqm.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sqm.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.telemetry.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 redir.metaservices.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 choice.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 choice.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 reports.wes.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 wes.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 services.wes.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sqm.df.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.ppe.telemetry.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.appex.bing.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.urs.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 telemetry.appex.bing.net:443" | Out-File -encoding ASCII -append $file
+"127.0.0.1 settings-sandbox.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 vortex-sandbox.data.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 survey.watson.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.live.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 watson.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 statsfe2.ws.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 corpext.msitadfs.glbdns2.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 compatexchange.cloudapp.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 cs1.wpc.v0cdn.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-0001.a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-0002.a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-0003.a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-0004.a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-0005.a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-0006.a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-0007.a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-0008.a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-0009.a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a-msedge.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 statsfe2.update.microsoft.com.akadns.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sls.update.microsoft.com.akadns.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 fe2.update.microsoft.com.akadns.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 diagnostics.support.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 corp.sts.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 statsfe1.ws.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 pre.footprintpredict.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 i1.services.social.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 i1.services.social.microsoft.com.nsatc.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 feedback.windows.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 feedback.microsoft-hohm.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 feedback.search.microsoft.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 live.rads.msn.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 ads1.msn.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 static.2mdn.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 g.msn.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 a.ads2.msads.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 b.ads2.msads.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 ad.doubleclick.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 ac3.msn.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 rad.msn.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 msntest.serving-sys.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 bs.serving-sys.com1" | Out-File -encoding ASCII -append $file
+"127.0.0.1 flex.msn.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 ec.atdmt.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 cdn.atdmt.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 db3aqu.atdmt.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 cds26.ams9.msecn.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 sO.2mdn.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 aka-cdn-ns.adtech.de" | Out-File -encoding ASCII -append $file
+"127.0.0.1 secure.flashtalking.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 adnexus.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 adnxs.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 *.rad.msn.com" | Out-File -encoding ASCII -append $file
+"127.0.0.1 *.msads.net" | Out-File -encoding ASCII -append $file
+"127.0.0.1 *.msecn.net" | Out-File -encoding ASCII -append $file
+
+Write-Host "Complete"
